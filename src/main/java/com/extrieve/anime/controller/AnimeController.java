69dca73839d17f384217addb70aae7f1f9485d3a
Nick <nicolas.cinera@gmail.com>
2022-06-24 22:49:59 -0500

    Simple Logging

--- src/main/java/com/extrieve/anime/controller/AnimeController.java
+++ src/main/java/com/extrieve/anime/controller/AnimeController.java
@@ -3,6 +3,7 @@ package com.extrieve.anime.controller;
 import com.extrieve.anime.entity.Anime;
 import com.extrieve.anime.exception.ResourceNotFoundException;
 import com.extrieve.anime.service.AnimeServiceImpl;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +20,18 @@ public class AnimeController {
     @Autowired
     private AnimeServiceImpl animeService;
 
+    Logger logger;
+
     @GetMapping("/all")
     public ResponseEntity<Collection<Anime>> getAllAnime(){
+        logger.info("Fetching all anime");
         Collection<Anime> allAnime = animeService.getAll();
         return ResponseEntity.ok(allAnime);
     }
 
     @GetMapping("/anime/{id}")
     public ResponseEntity<Anime> getAnimeById(@PathVariable String id){
+        logger.info("Fetching anime by ID");
         try {
         Anime anime = animeService.findByAnimeId(id);
         return ResponseEntity.ok(anime);
