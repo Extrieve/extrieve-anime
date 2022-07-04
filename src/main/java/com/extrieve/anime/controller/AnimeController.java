c01d6e9b4f30c4c8ffd360ab0af2f0a42d15351c
Nick <nicolas.cinera@gmail.com>
2022-07-04 17:35:32 -0500

    Update AnimeController.java

--- src/main/java/com/extrieve/anime/controller/AnimeController.java
+++ src/main/java/com/extrieve/anime/controller/AnimeController.java
@@ -13,7 +13,7 @@ import java.util.Collection;
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "http://localhost:4200")
+// @CrossOrigin(origins = "http://localhost:4200")
 public class AnimeController {
 
     @Autowired
